a6bd0ffe0aaf7a7ea10dafccdcbc6fee9526326c
HumphreyJ <joshhumphrey1@gmail.com>
2019-09-01 00:32:28 -0500

    Added a regex to use for searching for desired food

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const API_KEY ="3yOnYyLHj0C2QaKJKpwBSfq9E4WIP8DJvdbv1Xce"
 //DOM ELEMENTS
-const foodInput = document.querySelector('.food-input');
+const foodInput = document.querySelector('.food-input'),
+ foodOutput = document.querySelector('.food-output');
+ //DOM ELEMENTS
+
+ //VARIABLES
+ 
+ //VARIABLES
 
 function findFood() {
-    axios.get(` https://api.nal.usda.gov/ndb/V2/reports?ndbno=01009&ndbno=01009&type=b&format=json&api_key=${API_KEY}`).then(function(res) {
-        console.log(res.data.foods[0].food);
+    axios.get(` https://api.nal.usda.gov/ndb/V2/reports?ndbno=45212028&ndbno=45343301&type=b&format=json&api_key=${API_KEY}`).then(function(res) {
+        let foodName = res.data.foods[0].food.desc.name;
+        console.log(res.data.foods);
+
+        foodOutput.innerHTML = (`<p>${foodName}</p>`);
     })
 }
 
-function getInputValue(e) {
+function getMatches(wordToMatch, foods) {
     console.log(foodInput.value);
+    const regex = new RegExp(wordToMatch,'gi');
 }
 
-foodInput.addEventListener('keypress', getInputValue);
+foodInput.addEventListener('change', getMatches);
 findFood();
