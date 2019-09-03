345b3f3e9c0f01c2255039dd971f99844d464d08
Joshua Humphrey <joshhumphrey1@gmail.com>
2019-09-02 22:58:34 -0500

    Getting desired data from API now.

--- index.js
+++ index.js
@@ -5,22 +5,40 @@ const foodInput = document.querySelector('.food-input'),
  //DOM ELEMENTS
 
  //VARIABLES
- 
+ let searchTerm = foodInput.value;
+ ndbnoArr = [];
  //VARIABLES
+ 
 
 function findFood() {
-    axios.get(` https://api.nal.usda.gov/ndb/V2/reports?ndbno=45212028&ndbno=45343301&type=b&format=json&api_key=${API_KEY}`).then(function(res) {
-        let foodName = res.data.foods[0].food.desc.name;
-        console.log(res.data.foods);
+    axios.get(` https://api.nal.usda.gov/ndb/search/?format=json&q=eggs&max=25&offset=0&api_key=${API_KEY}`).then(function(res) {
+        // let foodName = res.data.foods[0].food.desc.name;
+        console.log(res.data);
+        console.log(res.data.list.item)
 
-        foodOutput.innerHTML = (`<p>${foodName}</p>`);
+        res.data.list.item.map(food => {
+            ndbnoArr.push(`ndbno=${food.ndbno}&`);
+        })
+
+        let = testString = ndbnoArr.toString().replace(/,/g, '');
+
+        console.log(testString);
+
+        axios.get(`https://api.nal.usda.gov/ndb/V2/reports?${testString}type=b&format=json&api_key=${API_KEY}`).then(function(res) {
+            console.log(res.data);
+            console.log(res.data.foods[0].food.desc.name);
+
+            res.data.foods.map(foods => {
+                console.log(foods.food.desc.name)
+            })
+        })
+
+        
+
+        // foodOutput.innerHTML = (`<p>${foodName}</p>`);
     })
 }
 
-function getMatches(wordToMatch, foods) {
-    console.log(foodInput.value);
-    const regex = new RegExp(wordToMatch,'gi');
-}
 
-foodInput.addEventListener('change', getMatches);
+
 findFood();
