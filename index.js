affbfe2b6d8ccbe8a8b15b34229b662a5676e530
Joshua Humphrey <joshhumphrey1@gmail.com>
2019-09-06 23:53:36 -0500

    Implemented basic search functionality. Data outputs in console.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const API_KEY ="3yOnYyLHj0C2QaKJKpwBSfq9E4WIP8DJvdbv1Xce"
 //DOM ELEMENTS
 const foodInput = document.querySelector('.food-input'),
+searchButton = document.querySelector('.search-btn'),
  foodOutput = document.querySelector('.food-output');
  //DOM ELEMENTS
 
  //VARIABLES
- let searchTerm = foodInput.value;
+ let searchTerm = '';
  ndbnoArr = [];
  //VARIABLES
- 
+ function getSearchTerm() {
+     searchTerm = foodInput.value;
+     console.log(searchTerm);
+ }
 
 function findFood() {
-    axios.get(` https://api.nal.usda.gov/ndb/search/?format=json&q=eggs&max=25&offset=0&api_key=${API_KEY}`).then(function(res) {
+    axios.get(` https://api.nal.usda.gov/ndb/search/?format=json&q=${searchTerm}&max=24&offset=0&api_key=${API_KEY}`).then(function(res) {
         // let foodName = res.data.foods[0].food.desc.name;
         console.log(res.data);
         console.log(res.data.list.item)
@@ -22,23 +26,27 @@ function findFood() {
 
         let = testString = ndbnoArr.toString().replace(/,/g, '');
 
-        console.log(testString);
+        // console.log(testString);
 
         axios.get(`https://api.nal.usda.gov/ndb/V2/reports?${testString}type=b&format=json&api_key=${API_KEY}`).then(function(res) {
             console.log(res.data);
             console.log(res.data.foods[0].food.desc.name);
 
             res.data.foods.map(foods => {
-                console.log(foods.food.desc.name)
+                // console.log(foods.food.desc.name)
+                
             })
         })
 
         
 
-        // foodOutput.innerHTML = (`<p>${foodName}</p>`);
+         
     })
+    
 }
 
+searchButton.addEventListener('click', findFood);
+// foodInput.addEventListener('keypress', getSearchTerm)
+foodInput.addEventListener('keyup', getSearchTerm)
 
-
-findFood();
+// findFood();
